bffdee0fc726436af1bc2e63c3ed17ade53341b2
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-02-01 09:47:22 -0700

    Adds handling of post requests to /orders

--- src/main/java/io/github/stephenlindstrom/taco_cloud/OrderController.java
+++ src/main/java/io/github/stephenlindstrom/taco_cloud/OrderController.java
@@ -2,6 +2,7 @@ package io.github.stephenlindstrom.taco_cloud;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -13,8 +14,17 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+  
   @GetMapping("/current")
   public String orderForm() {
     return "orderForm";
   }
+
+  @PostMapping
+  public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    log.info("Order submitted: {}", order);
+    sessionStatus.setComplete();
+    return "redirect:/";
+  }
+
 }
