e83cf16342e502e02cae75ff984d6a34f47173de
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-02-01 08:43:59 -0700

    Add controller handler method for post requests

--- src/main/java/io/github/stephenlindstrom/taco_cloud/DesignTacoController.java
+++ src/main/java/io/github/stephenlindstrom/taco_cloud/DesignTacoController.java
@@ -57,6 +57,13 @@ public class DesignTacoController {
     return "design";
   }
 
+  @PostMapping
+  public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    tacoOrder.addTaco(taco);
+    log.info("Processing taco: {}", taco);
+    return "redirect:/orders/current";
+  }
+  
   private Iterable<Ingredient> filterByType (List<Ingredient> ingredients, Type type) {
     return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
   }
