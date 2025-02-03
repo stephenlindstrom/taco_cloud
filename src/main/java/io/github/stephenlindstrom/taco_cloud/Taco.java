23375dc2c9238fb4c1ab845407bd0277d26a92ad
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-02-03 06:38:55 -0700

    Adds validation checks to Taco and TacoOrder

--- src/main/java/io/github/stephenlindstrom/taco_cloud/Taco.java
+++ src/main/java/io/github/stephenlindstrom/taco_cloud/Taco.java
@@ -1,9 +1,17 @@
 package io.github.stephenlindstrom.taco_cloud;
 import java.util.List;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+
+  @NotNull
+  @Size(min=5, message="Name must be at least 5 characters long")
   private String name;
+
+  @NotNull
+  @Size(min=1, message="You must choose at least 1 ingredient")
   private List<Ingredient> ingredients;
 }
