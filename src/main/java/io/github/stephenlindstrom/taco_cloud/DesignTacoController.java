596d779ccd8965756b5f1a21070d164e5277b183
Stephen Lindstrom <stphnlindstrom@gmail.com>
2025-02-04 07:19:50 -0700

    Inject JdbcIngredientRepository into DesignTacoController

--- src/main/java/io/github/stephenlindstrom/taco_cloud/DesignTacoController.java
+++ src/main/java/io/github/stephenlindstrom/taco_cloud/DesignTacoController.java
@@ -3,7 +3,9 @@ package io.github.stephenlindstrom.taco_cloud;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,20 +25,17 @@ import io.github.stephenlindstrom.taco_cloud.Ingredient.Type;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+  private final IngredientRepository ingredientRepo;
+
+  public DesignTacoController(IngredientRepository ingredientRepo) {
+    this.ingredientRepo = ingredientRepo;
+  }
+
   @ModelAttribute
   public void addIngredientsToModel(Model model) {
-    List<Ingredient> ingredients = Arrays.asList(
-      new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-      new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-      new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-      new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-      new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-      new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-      new Ingredient("CHED", "Cheddar", Type.CHEESE),
-      new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-      new Ingredient("SLSA", "Salsa", Type.SAUCE),
-      new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-    );
+    List<Ingredient> ingredients = StreamSupport
+      .stream(ingredientRepo.findAll().spliterator(), false)
+      .collect(Collectors.toList());
 
     Type[] types = Ingredient.Type.values();
     for (Type type : types) {
